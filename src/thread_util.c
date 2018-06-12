5fe4d8bd8a73c0b3d2513f2caf4371ee591760fc
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-12 13:09:50 -0500

    I am adopting the semaphore implementation from lwip, it is easily ported to other operating systems or platforms

--- src/thread_util.c
+++ src/thread_util.c
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdint.h>
 #include <pthread.h>
 #include <errno.h>
 
--- src/thread_util.h
+++ src/thread_util.h
@@ -1,7 +1,12 @@
 #ifndef THREAD_UTIL_H
 #define THREAD_UTIL_H
 
+#include <stdint.h>
 
-
+struct sys_sem;
+int32_t sys_sem_new(struct sys_sem **sem, uint8_t count);
+void sys_sem_signal(struct sys_sem **s);
+uint32_t sys_arch_sem_wait(struct sys_sem **s, uint32_t timeout);
+void sys_sem_free(struct sys_sem **sem);
 
 #endif
