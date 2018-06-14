f0de4ad3dd7cdbfdaf91da8909110009503beb08
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-14 13:49:56 -0500

    Update test cases

--- examples/test1.c
+++ examples/test1.c
@@ -3,50 +3,53 @@
 
 static thread_safe_queue* q; 
 
-int main(){
-   q = new_queue(10); 
+void consumer(void *arg){
+   blob* tmp;
+   while(1){
+      tmp = dequeue(q);
+      printf("%s\n", tmp->data);
+      printf("number_of_node %d\n", q->number_of_node);
+      free_blob(tmp);
+   }
+}
+
+void producer(void *arg){
    char testdata1[30]="testdata1!";
    char testdata2[30]="testdata2!";
    char testdata3[30]="testdata3!";
    char testdata4[30]="testdata4!";
-   blob* b1 = new_blob_and_move_data(1, 30, (uint8_t*)testdata1);
+   blob* b1 = new_blob_and_copy_data(1, 30, (uint8_t*)testdata1);
    blob* b2 = new_blob_and_move_data(2, 30, (uint8_t*)testdata2);
    blob* b3 = new_blob_and_move_data(3, 30, (uint8_t*)testdata3);
-   blob* b4 = new_blob_and_move_data(4, 30, (uint8_t*)testdata4);
+   blob* b4 = new_blob_and_copy_data(4, 30, (uint8_t*)testdata4);
    blob* b5 = new_blob_and_alloc_data(5, 30);
    strcpy((char*)b5->data, "testdata5!");
 
    enqueue(q, b1);
    enqueue(q, b2);
-   printf("number_of_node %d\n", q->number_of_node);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
    enqueue(q, b3);
    enqueue(q, b4);
-   printf("number_of_node %d\n", q->number_of_node);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
+
    enqueue(q, b3);
-   printf("number_of_node %d\n", q->number_of_node);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
    enqueue(q, b2);
-   printf("number_of_node %d\n", q->number_of_node);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
    enqueue(q, b1);
-   printf("number_of_node %d\n", q->number_of_node);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
    enqueue(q, b5);
-   printf("number_of_node %d\n", q->number_of_node);
-   printf("number_of_node %d, %s\n", q->number_of_node, dequeue(q)->data);
-   printf("number_of_node %d\n", q->number_of_node);
-   printf("try_dequeue %d\n", (try_dequeue(q)==NULL));
 
    free_blob(b1);
    free_blob(b2);
    free_blob(b3);
    free_blob(b4);
    free_blob(b5);
+}
 
+
+
+int main(){
+   q = new_queue(20); 
+   sys_thread_t t1 = sys_thread_new("consumer", consumer, NULL, 0, 0);
+   sys_thread_t t2 = sys_thread_new("producer", producer, NULL, 0, 0);
+   sys_thread_join(t1);
+   sys_thread_join(t2);
    return 0;
 
 }
