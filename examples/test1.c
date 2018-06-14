17fe6def02330f308ef8382f83056d8c4994c817
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-13 20:31:11 -0500

    update tests

--- examples/test1.c
+++ examples/test1.c
@@ -1,8 +1,10 @@
 #include "darkiot.h"
+#include <string.h>
+
+static thread_safe_queue* q; 
 
 int main(){
-
-   thread_safe_queue* q = new_queue(10); 
+   q = new_queue(10); 
    char testdata1[30]="testdata1!";
    char testdata2[30]="testdata2!";
    char testdata3[30]="testdata3!";
@@ -11,6 +13,10 @@ int main(){
    blob* b2 = new_blob_and_move_data(2, 30, (uint8_t*)testdata2);
    blob* b3 = new_blob_and_move_data(3, 30, (uint8_t*)testdata3);
    blob* b4 = new_blob_and_move_data(4, 30, (uint8_t*)testdata4);
+   blob* b5 = new_blob_and_alloc_data(5, 30);
+   strcpy((char*)b5->data, "testdata5!");
+
+
 
    enqueue(q, b1);
    enqueue(q, b2);
@@ -26,6 +32,18 @@ int main(){
    printf("%s\n", dequeue(q)->data);
    printf("%s\n", dequeue(q)->data);
    printf("%s\n", dequeue(q)->data);
+   enqueue(q, b5);
+   printf("%s\n", dequeue(q)->data);
+
+   free_blob(b1);
+   free_blob(b2);
+   free_blob(b3);
+   free_blob(b4);
+   free_blob(b5);
+
+
+
+
    return 0;
 
 }
