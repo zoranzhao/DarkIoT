ff13f5d235b1f1b586bce6ca6fca330933c5ee9e
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-14 22:06:49 -0500

    update test case

--- examples/test1.c
+++ examples/test1.c
@@ -5,7 +5,8 @@ static thread_safe_queue* q;
 
 void consumer(void *arg){
    blob* tmp;
-   while(1){
+   uint32_t i;
+   for(i = 0; i < 8; i++){
       tmp = dequeue(q);
       printf("%s\n", tmp->data);
       printf("number_of_node %d\n", q->number_of_node);
@@ -29,19 +30,23 @@ void producer(void *arg){
    enqueue(q, b2);
    enqueue(q, b3);
    enqueue(q, b4);
+   free_blob(b4);
 
    enqueue(q, b3);
    enqueue(q, b2);
    enqueue(q, b1);
-   enqueue(q, b5);
-
    free_blob(b1);
    free_blob(b2);
    free_blob(b3);
-   free_blob(b4);
+
+   enqueue(q, b5);
    free_blob(b5);
 }
 
+void server_thread(void *arg){
+   int mapreduce_service = service_init(8080, TCP);
+   printf("Service number is %d\n", mapreduce_service);
+}
 
 int main(){
    q = new_queue(20); 
@@ -49,7 +54,10 @@ int main(){
    sys_thread_t t2 = sys_thread_new("producer", producer, NULL, 0, 0);
    sys_thread_join(t1);
    sys_thread_join(t2);
-   sys_thread_join(t3);
-   return 0;
+   blob* temp = write_file_to_blob("test.jpg");
+   write_blob_to_file("out.jpg", temp);
+   free(temp);
 
+ 
+   return 0;
 }
