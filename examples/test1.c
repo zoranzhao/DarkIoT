be3752db491bde9eba45f0e17d157082becc6b24
Zhuoran Zhao <zoranzhao@gmail.com>
2018-06-22 14:18:16 -0500

    Update test cases

--- examples/test1.c
+++ examples/test1.c
@@ -5,10 +5,9 @@ static thread_safe_queue* q;
 
 void consumer(void *arg){
    blob* tmp;
-   uint32_t i;
-   for(i = 0; i < 8; i++){
+   while(1){
       tmp = dequeue(q);
-      printf("%s\n", tmp->data);
+      printf("ID: %d, Data: %s\n", tmp->id, tmp->data);
       printf("number_of_node %d\n", q->number_of_node);
       free_blob(tmp);
    }
@@ -27,18 +26,13 @@ void producer(void *arg){
    strcpy((char*)b5->data, "testdata5!");
 
    enqueue(q, b1);
+   free_blob(b1);
    enqueue(q, b2);
+   free_blob(b2);
    enqueue(q, b3);
+   free_blob(b3);
    enqueue(q, b4);
    free_blob(b4);
-
-   enqueue(q, b3);
-   enqueue(q, b2);
-   enqueue(q, b1);
-   free_blob(b1);
-   free_blob(b2);
-   free_blob(b3);
-
    enqueue(q, b5);
    free_blob(b5);
 }
@@ -151,16 +145,21 @@ void client_thread(void *arg){
    free_blob(temp);
 }
 
-void test_network_api(int argc, char **argv){
+void test_queue(int argc, char **argv){
    q = new_queue(20); 
    sys_thread_t t1 = sys_thread_new("consumer", consumer, NULL, 0, 0);
    sys_thread_t t2 = sys_thread_new("producer", producer, NULL, 0, 0);
-   sys_thread_t t3 = sys_thread_new("server", server_thread, NULL, 0, 0);
-   sys_thread_t t4 = sys_thread_new("client", client_thread, NULL, 0, 0);
    sys_thread_join(t1);
    sys_thread_join(t2);
-   sys_thread_join(t3);
-   sys_thread_join(t4);
+   
+}
+
+void test_network_api(int argc, char **argv){
+   q = new_queue(20); 
+   sys_thread_t t1 = sys_thread_new("server", server_thread, NULL, 0, 0);
+   sys_thread_t t2 = sys_thread_new("client", client_thread, NULL, 0, 0);
+   sys_thread_join(t1);
+   sys_thread_join(t2);
    
 }
 
@@ -219,7 +218,8 @@ void test_start_ctrl(int argc, char **argv)
 }
 
 int main(int argc, char **argv){
-   test_start_ctrl(argc, argv);
+   /*test_start_ctrl(argc, argv);*/
+   test_queue(argc, argv);
    return 0;
 }
 
