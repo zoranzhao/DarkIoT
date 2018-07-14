b8bba21867c7fbe7b20c2f9d8f6301016af18598
Zhuoran Zhao <zoranzhao@gmail.com>
2018-07-13 19:12:51 -0500

    ...

--- src/global_context.c
+++ src/global_context.c
@@ -1,22 +1,41 @@
-#include "global_queues.h"
+#include "global_context.h"
+/*Queues used in gateway device*/
+/*
 thread_safe_queue** results_pool;
 thread_safe_queue* ready_pool;
 uint32_t* results_counter;
 thread_safe_queue* registration_list;
+*/
+
+/*Queues used in edge device*/
+/*
 thread_safe_queue* task_queue;
 thread_safe_queue* result_queue;
+*/
 
-void init_queues(uint32_t cli_num){
-   results_pool = (thread_safe_queue**)malloc(sizeof(thread_safe_queue*)*cli_num);
-   results_counter = (uint32_t*)malloc(sizeof(uint32_t)*cli_num);
+static const char* addr_list[CLI_NUM] = EDGE_ADDR_LIST;
+
+void init_context(device_ctxt* ctxt, uint32_t cli_id, uint32_t cli_num){
    uint32_t i;
+   ctxt->results_pool = (thread_safe_queue**)malloc(sizeof(thread_safe_queue*)*cli_num);
+   ctxt->results_counter = (uint32_t*)malloc(sizeof(uint32_t)*cli_num);
    for(i = 0; i < cli_num; i++){
-      results_pool[i] = new_queue(MAX_QUEUE_SIZE);
-      results_counter[i] = 0;
+      ctxt->results_pool[i] = new_queue(MAX_QUEUE_SIZE);
+      ctxt->results_counter[i] = 0;
    } 
-   ready_pool = new_queue(MAX_QUEUE_SIZE); 
-   registration_list = new_queue(MAX_QUEUE_SIZE); 
+   ctxt->ready_pool = new_queue(MAX_QUEUE_SIZE); 
+   ctxt->registration_list = new_queue(MAX_QUEUE_SIZE); 
 
-   task_queue = new_queue(MAX_QUEUE_SIZE);
-   result_queue = new_queue(MAX_QUEUE_SIZE); 
+   ctxt->task_queue = new_queue(MAX_QUEUE_SIZE);
+   ctxt->result_queue = new_queue(MAX_QUEUE_SIZE); 
+
+   ctxt->this_cli_id = cli_id;
+   ctxt->total_cli_num = cli_num;
+
+/*
+   ctxt->addr_list[CLI_NUM];
+   for(i = 0; i < cli_num; i++){
+      
+   }
+*/ 
 }
--- src/global_context.h
+++ src/global_context.h
@@ -1,16 +1,23 @@
-#ifndef GLOBAL_QUEUES_H
-#define GLOBAL_QUEUES_H
+#ifndef GLOBAL_CONTEXT_H
+#define GLOBAL_CONTEXT_H
 #include "thread_safe_queue.h"
 #include "config.h"
 
-extern thread_safe_queue** results_pool;
-extern thread_safe_queue* ready_pool;
-extern uint32_t* results_counter;
-extern thread_safe_queue* registration_list;
-extern thread_safe_queue* task_queue;
-extern thread_safe_queue* result_queue;
+typedef struct dev_ctxt {
+   thread_safe_queue** results_pool;
+   thread_safe_queue* ready_pool;
+   uint32_t* results_counter;
+   thread_safe_queue* registration_list;
+   char* addr_list[CLI_NUM];
+   uint32_t total_cli_num;
 
-void init_queues(uint32_t cli_num);
+   thread_safe_queue* task_queue;
+   thread_safe_queue* result_queue; 
+   uint32_t this_cli_id;
+} device_ctxt;
+
+
+void init_context(device_ctxt* ctxt, uint32_t cli_num);
 
 #endif
 
